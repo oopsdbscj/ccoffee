6efebf3b114beee79fc0575adaced325700f8045
Justin Chen <chen.jc.19@pg.com>
2022-04-05 16:57:45 +0800

    changename changeyear

--- src/main/java/com/gouermazi/craw/Main.java
+++ src/main/java/com/gouermazi/craw/Main.java
@@ -6,7 +6,9 @@ import com.gouermazi.craw.refactoring.LinkHunter;
 import java.io.IOException;
 
 /**
- * @author jie·chen
+ * @author jie chen
+ * @year 2022
+ *
  */
 public class Main {
     public static void main(String[] args) throws IOException {
